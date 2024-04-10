7d1ddecc715fb404dc00ae1b16684ee8493ead18
nojaf <florian.verdonck@outlook.com>
2024-04-10 11:54:36 +0200

    Use Monaco editor as SyntaxHighlighter

--- dev-helpers/dev-helper-initializer.js
+++ dev-helpers/dev-helper-initializer.js
@@ -1,3 +1,51 @@
+import loader from "https://esm.sh/@monaco-editor/loader@1.4.0"
+
+const nojafPlugin = {
+  components: {
+    SyntaxHighlighter: (props) => {
+      const React = props.React;
+      const myRef = React.createRef();
+      React.useEffect(() => {
+        let editor = null;
+
+        const cancelable = loader.init();
+        cancelable.then(monaco => {
+          if (myRef.current) {
+            editor = monaco.editor.create(myRef.current, {
+              value: props.children,
+              language: props.language
+            })
+          }
+        }).catch(
+          (error) =>
+            error?.type !== 'cancelation' && console.error('Monaco initialization: error:', error),
+        );
+
+        return () => {
+          if (cancelable && !myRef.current) {
+            cancelable.cancel();
+          }
+
+          if (editor) {
+            console.log(editor);
+            editor.dispose();
+          }
+        }
+
+      }, [myRef, props.children]);
+
+
+      // The container needs a height.
+      const maxHeight = 450;
+      const newLineCount = !props.children ? 0 : (props.children.match(/\n/g)).length
+      const proposedHeight = (newLineCount * 14)
+      const minHeight = Math.max(100, Math.min(proposedHeight, maxHeight));
+
+      return React.createElement("div", { ref: myRef, style: { minHeight: `${minHeight}px` } }, null);
+    }
+  }
+}
+
 /* eslint-disable no-undef */
 window.onload = function() {
   window["SwaggerUIBundle"] = window["swagger-ui-bundle"]
@@ -11,7 +59,8 @@ window.onload = function() {
       SwaggerUIStandalonePreset
     ],
     plugins: [
-      SwaggerUIBundle.plugins.DownloadUrl
+      SwaggerUIBundle.plugins.DownloadUrl,
+      nojafPlugin
     ],
     // requestSnippetsEnabled: true,
     layout: "StandaloneLayout"
